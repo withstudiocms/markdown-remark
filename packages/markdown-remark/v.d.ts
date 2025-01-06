2b0ae426c87f81b553c4cb31428795584b4160d7
Adam Matthiesen <amatthiesen@outlook.com>
2025-01-05 22:27:44 -0800

    Refactor markdown-remark integration to update import paths

--- packages/markdown-remark/v.d.ts
+++ packages/markdown-remark/v.d.ts
@@ -1,6 +1,6 @@
 declare module 'studiocms:markdown-remark' {
-	export const Markdown: typeof import('../../../src/integration/markdown').Markdown;
-	export const render: typeof import('../../../src/integration/markdown').render;
+	export const Markdown: typeof import('./src/integration/markdown.js').Markdown;
+	export const render: typeof import('./src/integration/markdown.js').render;
 }
 
 declare module 'studiocms:markdown-remark/user-components' {
