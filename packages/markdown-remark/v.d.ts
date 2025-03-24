d05bec32b340594f2885b475f6e54e9e90000608
Adam Matthiesen <amatthiesen@outlook.com>
2025-03-23 22:44:34 -0700

    chore: update package dependencies and add knip configuration

--- packages/markdown-remark/v.d.ts
+++ packages/markdown-remark/v.d.ts
@@ -1,6 +1,8 @@
 declare module 'studiocms:markdown-remark' {
-	export const Markdown: typeof import('./src/integration/markdown.js').Markdown;
-	export const render: typeof import('./src/integration/markdown.js').render;
+	export const Markdown: typeof import('./src/virtual-components.js').Markdown;
+	export const render: typeof import('./src/virtual-components.js').render;
+	export type Props = import('./src/virtual-components.js').Props;
+	export type RenderResponse = import('./src/virtual-components.js').RenderResponse;
 }
 
 declare module 'studiocms:markdown-remark/user-components' {
