a0baddf69d5d4b58ccd6cdca6457e3e247930a34
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-22 00:48:40 -0800

    archive repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-packages/markdown-remark/README.md
+# MOVED
+
+This project has moved to the [`withstudiocms/studiocms`](https://github.com/withstudiocms/studiocms/) repository.
--- packages/markdown-remark/tests/fixture/astro/v.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'studiocms:markdown-remark' {
-	export const Markdown: typeof import('../../../src/virtual-components.js').Markdown;
-	export const render: typeof import('../../../src/virtual-components.js').render;
-}
-
-declare module 'studiocms:markdown-remark/user-components' {
-	export const componentMap: Record<string, string>;
-}
