58d43c2d3907df00cc35d6fe70886c0af88f44a8
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-22 20:48:58 -0800

    archive repo (#43)

--- vitest.workspace.js
+++
@@ -1,6 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-	'./packages/markdown-remark-processor/vitest.config.mts',
-	'./packages/markdown-remark/vitest.config.mts',
-]);
